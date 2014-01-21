8d27e265458045bb29bd961831ad1f0f80f0c870
Andrew Potapov <apotapov@gmail.com>
2014-01-20 20:24:59 -0600

    Removing extra files

--- core/src/main/java/com/pactstudios/games/tafl/core/es/model/map/objects/Piece.java
+++
@@ -1,8 +0,0 @@
-package com.pactstudios.games.tafl.core.es.model.map.objects;
-
-
-public class Piece {
-    public PieceType type;
-    public int x;
-    public int y;
-}
--- core/src/main/java/com/pactstudios/games/tafl/core/es/model/map/objects/Team.java
+++
@@ -1,6 +0,0 @@
-package com.pactstudios.games.tafl.core.es.model.map.objects;
-
-public enum Team {
-    WHITE,
-    BLACK
-}

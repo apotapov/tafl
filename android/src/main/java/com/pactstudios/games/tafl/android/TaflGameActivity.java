fb87bdead0fc9622219d12fa28027ac25320986a
Andrew Potapov <apotapov@gmail.com>
2014-01-22 14:35:23 -0600

    Getting the game to work on genimotion emulator as well.

--- android/src/main/java/com/pactstudios/games/tafl/android/TaflGameActivity.java
+++ android/src/main/java/com/pactstudios/games/tafl/android/TaflGameActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.pactstudios.games.tafl.core.TaflGame;
+import com.pactstudios.games.tafl.core.consts.Constants;
 
 public class TaflGameActivity extends AndroidApplication {
 
@@ -12,7 +13,7 @@ public class TaflGameActivity extends AndroidApplication {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        config.useGL20 = true;
+        config.useGL20 = Constants.GameConstants.USE_GL20;
         initialize(new TaflGame(), config);
     }
 }

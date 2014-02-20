3fee36933b8b92beb98b085c5cf0b5b0fbdf9b1c
Andrew Potapov <apotapov@gmail.com>
2014-02-19 18:04:06 -0600

    Locking the game in portrait mode.

--- android/src/main/java/com/pactstudios/games/tafl/android/TaflGameActivity.java
+++ android/src/main/java/com/pactstudios/games/tafl/android/TaflGameActivity.java
@@ -20,6 +20,7 @@ public class TaflGameActivity extends AndroidApplication {
         TaflGameConfig gameConfig = new TaflGameConfig();
         gameConfig.deviceType = DeviceType.PHONE;
 
+
         initialize(new TaflGame(gameConfig), config);
     }
 }

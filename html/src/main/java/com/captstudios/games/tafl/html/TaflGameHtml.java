0fbc5b586d8ee75252b3bf88751154b36b78f846
Andrew Potapov <apotapov@gmail.com>
2014-02-27 13:48:02 -0600

    Trying to get html5 going

--- html/src/main/java/com/captstudios/games/tafl/html/TaflGameHtml.java
+++ html/src/main/java/com/captstudios/games/tafl/html/TaflGameHtml.java
@@ -5,16 +5,23 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.captstudios.games.tafl.core.TaflGame;
 import com.captstudios.games.tafl.core.consts.Constants;
+import com.captstudios.games.tafl.core.utils.device.DeviceType;
+import com.captstudios.games.tafl.core.utils.device.TaflGameConfig;
 
 public class TaflGameHtml extends GwtApplication {
     @Override
     public ApplicationListener getApplicationListener () {
+
+        TaflGameConfig gameConfig = new TaflGameConfig();
+        gameConfig.deviceType = DeviceType.HTML;
+        gameConfig.vibrationSupported = false;
+
         return new TaflGame(null);
     }
 
     @Override
     public GwtApplicationConfiguration getConfig () {
         return new GwtApplicationConfiguration(
-                Constants.GameConstants.GAME_WIDTH, Constants.GameConstants.GAME_HEIGHT);
+                Constants.GameConstants.GAME_HTML_WIDTH, Constants.GameConstants.GAME_HTML_HEIGHT);
     }
 }
